05170f7eeaa95322421c63a58451923a8ad34ff5
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2022-12-16 12:51:33 +0100

    feat(product-navigation): mobile implementation

--- assets/product-navigation.js
+++ assets/product-navigation.js
@@ -1,3 +1,40 @@
+Function.prototype.throttle = function(minimumDistance) {
+  let timeout,
+      lastCalled = 0,
+      throttledFunction = this;
+
+  function throttleCore() {
+     let context = this;
+
+     function callThrottledFunction(args) {
+        lastCalled = Date.now();
+        throttledFunction.apply(context, args);
+     }
+     // Wartezeit bis zum nächsten Aufruf bestimmen
+     let timeToNextCall = minimumDistance - (Date.now() - lastCalled);
+     // Egal was kommt, einen noch offenen alten Call löschen
+     cancelTimer();
+     // Aufruf direkt durchführen oder um offene Wartezeit verzögern
+     if (timeToNextCall < 0) {
+        callThrottledFunction(arguments, 0);
+     } else {
+        timeout = setTimeout(callThrottledFunction, timeToNextCall, arguments);
+     }
+  }
+  function cancelTimer() {
+     if (timeout) {
+        clearTimeout(timeout);
+        timeout = undefined;
+     }
+  }
+  // Aufsperre aufheben und gepeicherte Rest-Aufrufe löschen
+  throttleCore.reset = function() {
+     cancelTimer();
+     lastCalled = 0;
+  }
+  return throttleCore;
+};
+
 class ProductNavigation extends HTMLElement {
   constructor() {
     super();
@@ -12,6 +49,8 @@ class ProductNavigation extends HTMLElement {
     this.originalPosition = this.navigation.offsetTop;
 
     this.onScrollHandler = this.onScroll.bind(this);
+    this.throttledScrollToActiveItem = this.scrollToActiveItem.throttle(300).bind(this);
+
     this.hideHeaderOnScrollUp = () => this.preventReveal = true;
 
     this.addEventListener('preventHeaderReveal', this.hideHeaderOnScrollUp);
@@ -46,12 +85,16 @@ class ProductNavigation extends HTMLElement {
     observer.observe(this.navigation);
   }
 
+  scrollToActiveItem(activeItem) {
+    this.navigation.querySelector("ul").scrollTo({left: activeItem.offsetLeft - 16, behavior: 'smooth'});
+  }
+
   onScroll() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if(scrollTop > this.originalPosition) {
       requestAnimationFrame(this.reveal.bind(this));  
-      let hasActiveItem = false;
+      let activeItem = false;
 
       // Reset navigation items
       this.navigationItems.forEach(n => n.parentElement.classList.remove('product-navigation__item--active'));
@@ -65,19 +108,22 @@ class ProductNavigation extends HTMLElement {
           const rect = targetElement.parentElement.getBoundingClientRect();
           if(rect.top <= this.navigation.offsetHeight) {
             // Set active class
-            navigationItem.parentElement.classList.add('product-navigation__item--active');
-            hasActiveItem = true;
+            activeItem = navigationItem.parentElement;
+            activeItem.classList.add('product-navigation__item--active');
             break;
           }
         }
       }
 
-      if(!hasActiveItem) {
-        const firstItem = this.navigation.querySelector('li');
-        if(firstItem) {
-          firstItem.classList.add('product-navigation__item--active');
+      if(!activeItem) {
+        activeItem = this.navigation.querySelector('li');
+        if(activeItem) {
+          activeItem.classList.add('product-navigation__item--active');
         }
       }
+
+      // Scroll to active item (mobile)
+      this.throttledScrollToActiveItem(activeItem);
     } else {
       requestAnimationFrame(this.hide.bind(this));  
     }
